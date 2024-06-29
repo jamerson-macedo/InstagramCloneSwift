3a3060a278172276aa9e7314f35fd37139853222
jamerson-macedo <jamerson_macedo@hotmail.com>
2024-06-28 21:20:48 -0300

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Jamerson Macedo on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
